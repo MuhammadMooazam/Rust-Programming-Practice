76e6b1ec99752278a19be4e8f216c5bd13ae8d63
Syed Muhammad Mooazam <muhammadmoazam29@gmail.com>
2022-01-02 20:12:53 +0500

    add folder

---
+++ 4) shadowing/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x = 9;
+    let y = x * 2;
+    println!("Value of x is {} and y is {}", x, y);
+    let x: f64 = 10.001;
+    println!("New value of x is {}", x);
+}
---
+++ 5) data_types_scalar/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // Scalar Data Types
+    let name = "Mooazam";
+    let age = 21;
+    let percentage = 82.24;
+    let passed = true;
+    println!(
+        "Name is {} and age is {} with percentage {} and he is passed which is {}",
+        name, age, percentage, passed
+    );
+
+    let secret_number: i32 = -210;
+    println!("Secret number is {}", secret_number);
+}
