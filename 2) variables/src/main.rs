35b0aa67859073f204c913bab21d0ea7b6dccd3f
Syed Muhammad Mooazam <muhammadmoazam29@gmail.com>
2022-01-02 20:11:34 +0500

    add folder

---
+++ 2) variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut temp = 35;
+    temp = 40;
+    let hum = 90;
+    println!(
+        "The temperature is {} degree C and Humidity is {} %",
+        temp, hum
+    );
+}
---
+++ 3) constant/src/main.rs
@@ -0,0 +1,4 @@
+const pi: f64 = 3.1415926535897432;
+fn main() {
+    println!("The value of pi is about {}", pi);
+}
